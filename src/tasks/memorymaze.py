70030c1bdcec0258561900e6b57a1e9e844d9bd4
Nikita-Dudorov <zoom_ccss@mail.ru>
2024-02-05 11:02:18 +0300

    wrap memorymaze env

--- src/tasks/memorymaze.py
+++ src/tasks/memorymaze.py
@@ -3,22 +3,26 @@ import os
 
 os.environ['MUJOCO_GL']='osmesa'
 
-# class GymToGymnasium(gym.Wrapper):
-#     def __init__(self, env: gym.Env):
-#         gym.Wrapper.__init__(self, env)
+class FrameProcessing(gym.Wrapper):
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
 
-#     def reset(self, seed=None, options=None):
-#         # TODO: pass seed on reset
-#         state = self.env.reset()
-#         info = {}
-#         return state, info
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        self._last_frame = obs.copy()
+        obs = obs.astype(float) / 255
+        return obs, info
     
-#     def step(self, action):
-#         state, reward, done, info = self.env.step(action)
-#         truncated = info.get('TimeLimit.truncated', False)
-#         return state, reward, done, truncated, info
+    def step(self, action):
+        obs, reward, done, truncated, info = self.env.step(action)
+        self._last_frame = obs.copy()
+        obs = obs.astype(float) / 255
+        return obs, reward, done, truncated, info
+    
+    def render(self):
+        return self._last_frame
 
 def create_memorymaze(env_name):
     env = gym.make(env_name)
-    # env = GymToGymnasium(env)
+    env = FrameProcessing(env)
     return env 
