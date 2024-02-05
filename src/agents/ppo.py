289ccae2e3e097dcfabd6bc41eb172745ab53e9f
Nikita-Dudorov <zoom_ccss@mail.ru>
2024-02-05 23:14:06 +0300

    ...

--- src/agents/ppo.py
+++ src/agents/ppo.py
@@ -23,7 +23,7 @@ class PPOAgent(BaseAgent):
                         actor_fn:Callable,critic_fn:Callable,optimizer:optax.GradientTransformation,
                          num_steps=128, anneal_lr=True, gamma=0.99, lr_schedule=optax.linear_schedule,
                         gae_lambda=0.95, num_minibatches=4, update_epochs=4, norm_adv=True,
-                        clip_coef=0.1, value_clip_coef=0.0, ent_schedule=optax.Schedule, vf_coef=0.5, max_grad_norm=0.5,
+                        clip_coef=0.1, value_clip_coef=None, ent_schedule=optax.Schedule, vf_coef=0.5, max_grad_norm=0.5,
                         target_kl=None,sequence_length=None,continuous_actions=False) -> None:
 
         super(PPOAgent,self).__init__(train_envs=train_envs,eval_env=eval_env,rollout_len=num_steps,repr_model_fn=repr_model_fn,seq_model_fn=seq_model_fn,
@@ -117,7 +117,7 @@ class PPOAgent(BaseAgent):
                 pg_loss = jnp.maximum(pg_loss1, pg_loss2).mean()
 
                 # Value loss
-                if self.value_clip_coef != 0:
+                if self.value_clip_coef is not None:
                     values_new = mb_returns + jnp.clip(values_new - mb_returns, -self.value_clip_coef, self.value_clip_coef)
                 v_loss = 0.5 * ((values_new - mb_returns) ** 2).mean()
 
